131fa6e255c6225effe91f6755e48c7eb5db3a0f
Tharindu Silva <78898203+TharinduSilva2019774@users.noreply.github.com>
2023-05-06 21:31:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [newData, setNewData] = useState();
+
+  const [message, setMassage] = useState();
+  const [userId, setUserId] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://messenger-backend-production.up.railway.app/getAll", {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setNewData(data);
+        });
+      console.log(process.env);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const topicStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10hv",
+    fontFamily: "Arial",
+  };
+  const bodyStyle = {
+    color: "white",
+    backgroundColor: "#154c79",
+    padding: "5px",
+    fontFamily: "Arial",
+    height: "90vh",
+  };
+  function textareaChange(event) {
+    setMassage(event.target.value);
+  }
+
+  function userIdChange(event) {
+    setUserId(event.target.value);
+  }
+
+  function sending() {
+    console.log(message);
+    console.log(userId);
+    if (message != "") {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId: userId, message: message }),
+      };
+      fetch(
+        "https://messenger-backend-production.up.railway.app/send",
+        requestOptions
+      ).then((response) => console.log(response));
+
+      setMassage("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header style={topicStyle} className>
+        Silent Eye Nexus (SEN)
       </header>
+      <body style={bodyStyle}>
+        <div className="scrollable-div">
+          {newData?.map((message) => (
+            <div>
+              {message.user.name} :: {message.messageBody}{" "}
+            </div>
+          ))}
+        </div>
+        <div>
+          <textarea
+            placeholder="Enter your message"
+            value={message}
+            onChange={textareaChange}
+          />
+          <button onClick={sending}> Send </button>
+        </div>
+        <input
+          type="text"
+          onChange={userIdChange}
+          placeholder="Plz put your ID"
+        />
+      </body>
     </div>
   );
 }
